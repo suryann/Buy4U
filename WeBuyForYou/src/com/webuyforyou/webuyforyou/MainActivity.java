1dc23a9a23e425238085b71806fe562b8327645e
S0045430 <soorianarayanan.balagar@wipro.com>
2014-09-25 16:12:31 +0530

    implemented Actionbarsherlock

--- WeBuyForYou/src/com/webuyforyou/webuyforyou/MainActivity.java
+++ WeBuyForYou/src/com/webuyforyou/webuyforyou/MainActivity.java
@@ -1,14 +1,19 @@
 package com.webuyforyou.webuyforyou;
 
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+
+import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
 import android.widget.ListView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends SherlockActivity {
   ListView list;
+  String [] SelectText = {"Save", "Settings"};
   String[] web = {
     "Google Plus",
       "Twitter",
@@ -30,6 +35,8 @@ public class MainActivity extends Activity {
   };
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+      setTheme(SampleList.THEME); //Used for theme switching in samples
+
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     CustomList adapter = new
@@ -44,4 +51,41 @@ public class MainActivity extends Activity {
                 }
             });
   }
+  
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+      //Used to put dark icons on light action bar
+      boolean isLight = SampleList.THEME == R.style.Theme_Sherlock_Light;
+
+      menu.add(SelectText[0]).setTitle("Add")
+          .setIcon(R.drawable.ic_action_content_new)
+          .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+
+      menu.add(SelectText[1]).setTitle("Settings")
+          .setIcon(R.drawable.ic_action_action_settings)
+          .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM | MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+
+      return true;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+      // If this callback does not handle the item click, onPerformDefaultAction
+      // of the ActionProvider is invoked. Hence, the provider encapsulates the
+      // complete functionality of the menu item.
+	  if (item.getTitle().equals("Settings"))
+		  startActivity(new Intent(MainActivity.this, Preference.class));
+	  else
+		  Toast.makeText(this, "Handling in onOptionsItemSelected avoided",Toast.LENGTH_SHORT).show();
+	  
+      return false;
+  }
+
+  
 }
